e1fb0337cb6e83e4c04f83fc66c6c1d4b60dcc56
Zhansaya Aralkhan <aralkhanzhansaya@gmail.com>
2024-09-04 21:12:15 +0500

    :qw

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const menu = document.querySelector('#menu');
 icon.addEventListener('click', () => {
     menu.classList.toggle('disp');
 });
+
+const sort = document.querySelector('#sort');
+const by = document.querySelector('#by');
+
+sort.addEventListener('click', () => {
+    by.classList.toggle('disp');
+});
+
+const sorttype = document.querySelector('#sort-type');
+const bytype = document.querySelector('#by-type');
+
+sorttype.addEventListener('click', () => {
+    bytype.classList.toggle('disp');
+});
